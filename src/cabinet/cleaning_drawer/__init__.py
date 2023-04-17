10840badfd0c16ba44a38ea88073bd6e62b891ec
Nick Anthony <nanthony007@gmail.com>
2023-04-17 08:49:39 -0700

    Add cleaning drawer

---
+++ src/cabinet/cleaning_drawer/__init__.py
@@ -0,0 +1,4 @@
+"""This drawer exists for cleaning functions.
+
+It's a bit of a catch-all for functions that don't fit in any other drawer.
+"""
--- src/cabinet/cleaning_drawer/normalize.py
+++ src/cabinet/cleaning_drawer/normalize.py
@@ -1,9 +1,31 @@
+"""This module contains code for typical data normalization tasks.
+
+We generally enforce 'type-saftey' by using pydantic's validate_arguments decorator
+and other pydantic types.
+"""
 from pydantic import validate_arguments
 from pydantic.types import PositiveInt
 
 
 @validate_arguments
 def categorize_age(age: PositiveInt) -> str:
+    """Categorize an age into a string.
+
+    Args:
+        age (PositiveInt): Age to categorize. Must be a positive integer.
+
+    Returns:
+        str: Categorized age.
+
+    Examples:
+        >>> categorize_age(10)
+        '<18'
+        >>> categorize_age(18)
+        '18-25'
+        >>> # a general example using pandas
+        >>> df['age'].apply(categorize_age)
+        pandas.Series(['<18', '18-25', '26-35', '36-45', '46-55', '56-65', '65+'])
+    """
     if age < 18:
         return "<18"
     elif age <= 25:
