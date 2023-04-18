f37c04e13d767e4190ab06ee964335b3252c9264
Nick Anthony <nanthony007@gmail.com>
2023-04-17 21:19:01 -0700

    Add some awfully written docstrings

--- scripts/generate_cui_to_snomed_map.py
+++ scripts/generate_cui_to_snomed_map.py
@@ -14,12 +14,30 @@ LINE_PARTS = list[str]
 
 
 def load_lines(path: Path) -> Iterable[LINE_PARTS]:
+    """Load the lines from the MRCONSO.RRF file.
+
+    More information on the file format can be found [here](https://www.ncbi.nlm.nih.gov/books/NBK9685/table/ch03.T.concept_names_and_sources_file_mr/)
+    """
     with open(path, "r") as f:
         for line in f:
             yield line.split("|")
 
 
 def filter_lines(parts: LINE_PARTS) -> None | LINE_PARTS:
+    """Filter the lines to only include the ones we want.
+
+    We want the following:
+        - SNOMEDCT_US
+        - Active
+        - Preferred term
+        - Fully specified name
+
+    Args:
+        parts (LINE_PARTS): The line parts to filter
+
+    Returns:
+        None | LINE_PARTS: The filtered line parts or None if the line should be filtered out
+    """
     if parts[11] != "SNOMEDCT_US":
         return None
     if parts[6] != "Y":
@@ -34,6 +52,14 @@ def filter_lines(parts: LINE_PARTS) -> None | LINE_PARTS:
 
 
 def make_maps(lines: Iterable[LINE_PARTS]) -> CONCEPT_MAP:
+    """Make the maps from the lines.
+
+    Args:
+        lines (Iterable[LINE_PARTS]): The lines to make the maps from
+
+    Returns:
+        CONCEPT_MAP: The map from CUI to SNOMEDCT_US
+    """
     cui_to_snomed: CONCEPT_MAP = {}
     for line_parts in tqdm(
         filter(None, map(filter_lines, lines)), desc="Making Maps..."
@@ -45,6 +71,11 @@ def make_maps(lines: Iterable[LINE_PARTS]) -> CONCEPT_MAP:
 
 
 def write_map_to_file(cui_map: CONCEPT_MAP) -> None:
+    """Write the map to a file.
+
+    Args:
+        cui_map (CONCEPT_MAP): The map to write to a file
+    """
     dest_dir = Path().cwd() / "src" / "cabinet" / "data"
     dest_dir.mkdir(parents=True, exist_ok=True)
     cui_map_bytes = orjson.dumps(cui_map)
@@ -54,6 +85,7 @@ def write_map_to_file(cui_map: CONCEPT_MAP) -> None:
 
 
 def main():
+    """Main function to run the script."""
     lines = load_lines(Path().cwd().parent / "knowledge_base_data" / "MRCONSO.RRF")
     cui_to_snomed = make_maps(lines)
     console.log("[yellow]Writing maps...")
--- scripts/generate_snomed_tree.py
+++ scripts/generate_snomed_tree.py
@@ -14,28 +14,58 @@ console = Console()
 
 # type aliases because I'm lazy
 LINE_PARTS = list[str]
-CONCEPT_TREE = dict[str, set[str]]
+TREE = dict[str, set[str]]
 
 
 def load_lines(path: Path) -> Iterable[LINE_PARTS]:
+    """Load the lines from the SNOMEDCT_US relationship shapshot file.
+
+    More information on the file format can be found [here](https://confluence.ihtsdotools.org/display/DOCRELFMT/4.2.3+Relationship+File+Specification)
+
+    Args:
+        path (Path): The path to the file
+
+    Returns:
+        Iterable[LINE_PARTS]: The lines
+    """
     with open(path, "r") as f:
         for line in f:
             yield line.split("\t")
 
 
 def filter_lines(parts: LINE_PARTS) -> None | LINE_PARTS:
+    """Filter the lines to only include the ones we want.
+
+    We want the following:
+        - Active
+        - "is_a" relationship
+
+    This means that the relationship itself is active; however,
+    that implies both concepts involved are also active. This implication
+    is validated via tests.
+
+    Args:
+        parts (LINE_PARTS): The line parts to filter
+
+    Returns:
+        None | LINE_PARTS: The filtered line parts or None if the line should be filtered out
+    """
     # active and "is_a" relationship
     if parts[2] == "1" and parts[7] == "116680003":
         return parts
     return None
 
 
-def check_tree():
-    assert True
+def make_tree(lines: Iterable[LINE_PARTS]) -> TREE:
+    """Make the tree from the lines.
 
+    Args:
+        lines (Iterable[LINE_PARTS]): The lines to make the tree from
 
-def make_tree(lines: Iterable[LINE_PARTS]) -> CONCEPT_TREE:
-    tree: CONCEPT_TREE = {}
+    Returns:
+        CONCEPT_TREE: The tree
+    """
+    tree: TREE = {}
     for line_parts in tqdm(
         filter(None, map(filter_lines, lines)), desc="Making tree..."
     ):
@@ -47,7 +77,12 @@ def make_tree(lines: Iterable[LINE_PARTS]) -> CONCEPT_TREE:
     return tree
 
 
-def write_map_to_file(tree: CONCEPT_TREE) -> None:
+def write_tree_to_file(tree: TREE) -> None:
+    """Write the tree to a file.
+
+    Args:
+        map (CONCEPT_TREE): The map to write to a file
+    """
     dest_dir = Path().cwd() / "src" / "cabinet" / "data"
     dest_dir.mkdir(parents=True, exist_ok=True)
     tree_bytes = orjson.dumps({k: list(v) for k, v in tree.items()})
@@ -57,6 +92,7 @@ def write_map_to_file(tree: CONCEPT_TREE) -> None:
 
 
 def main():
+    """Main function to run the script."""
     lines = load_lines(
         Path().cwd().parent
         / "knowledge_base_data"
@@ -67,7 +103,7 @@ def main():
     )
     tree = make_tree(lines)
     console.log("[yellow]Writing tree...")
-    write_map_to_file(tree)
+    write_tree_to_file(tree)
     console.log("[green]Done!")
 
 
