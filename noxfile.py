1ddb8054b43e1516b8111460865940257be7d4f6
Nick Anthony <nanthony007@gmail.com>
2023-06-12 15:46:05 -0700

    Add CI config file

--- noxfile.py
+++ noxfile.py
@@ -1,23 +1,24 @@
-import tempfile
-from typing import Any
-
 # add type ignores since these are global imports
-from nox import session  # type: ignore
-from nox.sessions import Session  # type: ignore
+import nox  # type: ignore
 
 
-@session(python="3.11")
+@nox.session(python="3.11", reuse_venv=True)
 def lint(session):
-    session.run("poetry", "install", "--with=dev", external=True)
+    # we need the main dependencies to run mypy so it doesn't fail
+    # on imports
+    session.run("poetry", "install", "--only", "dev", external=True)
     session.run("black", ".", "--check")
     session.run("ruff", ".")
+    # ... , but we ignore that for now and use `--ignore-missing-imports`
     session.run(
         "mypy",
         ".",
+        "--ignore-missing-imports",
     )
 
 
-@session(python=["3.9", "3.10", "3.11"])
+# TODO: python 3.9 fails on match statement
+@nox.session(python=["3.9", "3.10", "3.11"], reuse_venv=True)
 def test(session):
     # ! In order for this to work, you need to have a local instance of the API running
     # ! and the MODE environment variable set to DEV in your .env file AND you need to
@@ -26,5 +27,5 @@ def test(session):
     # but for now we assume that the API is running and MetaMap is installed.
     # this will be good practice for integration tests... but to be honest
     # probably won't hold a lot of value right now without CI for windows as well...
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--without", "docs,dev", external=True)
     session.run("pytest")
